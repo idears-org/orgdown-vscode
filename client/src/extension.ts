ea0de07955539b98e0d74710d203e95a74d9d0b3
San <san-shangren.yi@outlook.com>
2025-08-01 12:08:18 +0800

    Refactor: checkpoint before the implementation of vscode-org-tm-grammar

---
+++ client/src/extension.ts
@@ -0,0 +1,52 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+} from 'vscode-languageclient/node';
+import * as path from 'path';
+
+let client: LanguageClient;
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  const serverModule = context.asAbsolutePath(
+    path.join('server', 'out', 'server.js'),
+  );
+  const serverOptions: ServerOptions = {
+    run: { module: serverModule, transport: TransportKind.ipc },
+    debug: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+      options: { execArgv: ['--nolazy', '--inspect=6009'] },
+    },
+  };
+
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: 'file', language: 'org' }],
+    synchronize: {
+      fileEvents: vscode.workspace.createFileSystemWatcher('**/.clientrc'),
+    },
+  };
+
+  client = new LanguageClient(
+    'orgdown',
+    'Orgdown Language Server',
+    serverOptions,
+    clientOptions,
+  );
+
+  client.start();
+}
+
+// This method is called when your extension is deactivated
+export function deactivate(): Thenable<void> | undefined {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
+}
---
+++ server/src/server.ts
@@ -0,0 +1,24 @@
+import {
+  createConnection,
+  TextDocuments,
+  ProposedFeatures,
+  InitializeParams,
+  TextDocumentSyncKind,
+  InitializeResult,
+} from 'vscode-languageserver/node';
+import { TextDocument } from 'vscode-languageserver-textdocument';
+
+const connection = createConnection(ProposedFeatures.all);
+const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
+
+connection.onInitialize((params: InitializeParams) => {
+  const result: InitializeResult = {
+    capabilities: {
+      textDocumentSync: TextDocumentSyncKind.Incremental,
+    },
+  };
+  return result;
+});
+
+documents.listen(connection);
+connection.listen();
